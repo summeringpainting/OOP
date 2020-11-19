6a127c2a4f346d7bf664a0b26ee30e05a75e1085
Stephen Summering <54336901+summeringpainting@users.noreply.github.com>
2020-11-19 12:56:20 -0500

    Add files via upload

--- oop.py
+++ oop.py
@@ -12,19 +12,88 @@ class Soldier:
         return '{} {}'.format(self.first, self.last)
 
     def whack(self):
-        self.strength = self.strength - Soldier.hit
+        self.strength = self.strength - self.hit
         return self.strength
-    
-    
-    
+
+    @classmethod
+    def set_hit(cls, amount):
+        cls.hit = amount
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+    def __str__(self):
+        return "Soldier('{}', '{}')".format(self.fullname(), self.weight)
+
+class Enemy(Soldier):
+    hit = 5
+
+    def __init__(self, first, last, weight, strength, type):
+        super().__init__(first, last, weight, strength)
+        self.type = type
+
+class Manager(Soldier):
+
+    def __init__(self, first, last, weight, strength, soldiers=None):
+        super().__init__(first, last, weight, strength)
+        if soldiers is None:
+            self.soldiers = []
+        else:
+            self.soldiers = soldiers
+
+    def add_sold(self, sold):
+        if sold not in self.soldiers:
+            self.soldiers.append(sold)
+
+    def remove_sold(self, sold):
+        if sold in self.soldiers:
+            self.soldiers.remove(sold)
+
+    def print_sold(self):
+        for sold in self.soldiers:
+            print('-->', sold.fullname())
+
+
+bad_1 = Enemy('big', 'loser', 1000, 30, 'squeek')
+bad_2 = Enemy('lil', 'baby', 1000, 30, 'chonker')
+
+print(bad_1)
+
+mgr_1 = Manager('boss', 'boi', 300, 700, [bad_1])
+
+print(issubclass(Manager, Enemy))
+
+print(mgr_1.weight)
+
+mgr_1.add_sold(bad_2)
+mgr_1.remove_sold(bad_1)
+
+mgr_1.print_sold()
+# print(help(Enemy))
+
 sold_1 = Soldier('joe', 'shmo', 600, 60)
 sold_2 = Soldier('bri', 'guy', 100, 60)
 sold_3 = Soldier('queen', 'bee', 60, 60)
+
+print(bad_1.type)
+bad_1.whack()
+print(bad_1.strength)
+
+import datetime
+my_date = datetime.date(2016, 7, 11)
+print(Soldier.is_workday(my_date))
 
 # print('{} {}'.format(sold_1.first, sold_1.last))
 
 # print(sold_1.fullname())
 # print(sold_1.whack())
+
+# print(Soldier.hit)
+# print(sold_1.hit)
+# print(sold_2.hit)
 
 dude = input('Do you want to be joe, queen or bri? ')
 
@@ -59,3 +128,6 @@ while sold_1.strength != 0 and sold_2.strength !=0 and sold_3.strength !=0:
              print('queen now has', sold_3.strength,'hp')
         
 print('You killed them!')
+
+
+YOOOOOOOOOO
